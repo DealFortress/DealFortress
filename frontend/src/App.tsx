558f83260bd9703fe54e15262580af607215ef68
Atte <pitkasenatte@gmail.com>
2023-06-01 10:18:03 +0200

    First fetch from frontend to api

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,23 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import { Navbar } from './component/Navbar'
 import { Footer } from './component/Footer'
 import { TradePage } from './pages/TradePage'
+import { GetSellAdsFromAPI } from './services/DealFortressAPI'
 
 function App() {
 
+  const [ sellAds, setSellAds ] = useState<SellAd[]>([]);
+
+  const GetData = async () => {
+    setSellAds(await GetSellAdsFromAPI());
+  }
+
+  useEffect(() => {
+    GetData();
+  }, [])
+
+  console.log(sellAds[0]);
 
   return (
     <>
@@ -15,5 +27,6 @@ function App() {
     </>
   )
 }
+
 
 export default App
--- frontend/src/services/DealFortressAPI.ts
+++ frontend/src/services/DealFortressAPI.ts
@@ -1,8 +1,8 @@
 
 // put in the env
-const sellAdUrl = "https://localhost:5000/";
+const sellAdUrl = "https://localhost:5000/api/SellAds";
 
-const GetSellAdsFromAPI = async () => {
+export const GetSellAdsFromAPI = async () => {
     const response = await fetch(sellAdUrl);
     return await response.json() as SellAd[];
 }
