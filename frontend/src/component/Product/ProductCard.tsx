82efd33470151cb8b9fe30bde970ac994b7c5c52
Atte <pitkasenatte@gmail.com>
2023-06-05 14:47:13 +0200

    removes tests and reinitializes .Net solution for API

--- frontend/src/component/Product/ProductCard.tsx
+++ frontend/src/component/Product/ProductCard.tsx
@@ -12,7 +12,7 @@ export const ProductCard = ({product} : Props) => {
   return (
     <Link to={`../sellads/${product.sellAdId}`}>
     <div className="card flex-basis flex-grow w-96">
-        <img src="hello" className="card-img-top h-40 w-full object-cover" alt="product image"/>
+        <img src={product.images[0].url} className="card-img-top h-40 w-full object-cover" alt="product image"/>
         <div className="card-body">
             <h5 className="card-title">{product.name}</h5>
             <p className="card-text">price: {product.price} receipt: {product.receipt} warranty: {product.warranty} category:{product.categoryId} condition:{product.condition}</p>
