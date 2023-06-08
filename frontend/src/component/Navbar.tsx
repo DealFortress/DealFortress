c3b087f4196de6c6a2142d724f0846f0d206c07f
luc siat <lucsiat@gmail.com>
2023-06-08 11:37:38 +0200

    add fontawesome to project

--- frontend/src/component/Navbar.tsx
+++ frontend/src/component/Navbar.tsx
@@ -1,11 +1,16 @@
 import { Link } from 'react-router-dom'
 import logo from '../assets/logo.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMagnifyingGlass, faHeart } from '@fortawesome/free-solid-svg-icons'
 
 export const Navbar = () => {
   return (
     <div className="w-screen py-4 top-0 relative bg-[rgb(42,42,42)] text-white">
-        <div>
+        <div className='flex justify-around'>
             <img src={logo} alt="deal fortress logo" className="logo-s"/>
+            <p>Deal Fortress</p>
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+            <FontAwesomeIcon icon={faHeart} />
         </div>
         <div className="container flex justify-between">
             <Link to="/Notices">Notices</Link>
