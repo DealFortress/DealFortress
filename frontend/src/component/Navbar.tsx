a1aee8fdf06b4e0e88423350d2a5143fe4ebbd6b
luc siat <lucsiat@gmail.com>
2023-06-08 11:24:18 +0200

    add logo

--- frontend/src/component/Navbar.tsx
+++ frontend/src/component/Navbar.tsx
@@ -1,12 +1,16 @@
 import { Link } from 'react-router-dom'
+import logo from '../assets/logo.png'
 
 export const Navbar = () => {
   return (
     <div className="w-screen py-4 top-0 relative bg-[rgb(42,42,42)] text-white">
-      <div className="container flex justify-between">
-        <Link to="/Notices">Notices</Link>
-        <Link to="/products">products</Link>
-      </div>
+        <div>
+            <img src={logo} alt="deal fortress logo" className="logo-s"/>
+        </div>
+        <div className="container flex justify-between">
+            <Link to="/Notices">Notices</Link>
+            <Link to="/products">products</Link>
+        </div>
     </div>
   )
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,8 +5,8 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+    },
   },
   plugins: [],
 }
-
