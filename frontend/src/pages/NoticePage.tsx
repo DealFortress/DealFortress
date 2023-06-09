7e3d8ab52328847e6c5386bdf31139bba0cf04de
luc siat <lucsiat@gmail.com>
2023-06-09 16:42:04 +0200

    style notice page

--- frontend/src/pages/NoticePage.tsx
+++ frontend/src/pages/NoticePage.tsx
@@ -2,7 +2,7 @@ import { Link, useParams } from "react-router-dom"
 import { Notice } from "../types"
 import { Main } from "../component/Main"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faBars, faCashRegister, faCity, faCross, faMoneyBill, faTractor, faTruckRampBox, faXmark } from "@fortawesome/free-solid-svg-icons"
+import { faCashRegister, faCity, faTruckRampBox, faXmark } from "@fortawesome/free-solid-svg-icons"
 
 type Props = {
   notices: Notice[]
@@ -24,10 +24,10 @@ export const NoticePage = ( {notices} : Props) => {
       <div className="flex flex-col gap-4">
         <section className="bg-darkblue relative rounded-lg p-6 flex flex-col justify-between gap-12 white-box-border">
             {/* make it a component */}
-            <div className="border border-b-2 absolute w-full mx-auto top-0 left-0 flex justify-between gap-2 px-2 text-2xl">
-                <p>{notice?.id}</p>
-                <span className="w-full flex flex-col gap-1 mx-1 my-auto" ><hr /><hr /><hr /></span>
-                <Link to="/" className="white-box-border"><FontAwesomeIcon className="text-end " icon={faXmark} /></Link>
+            <div className="border border-b-2 absolute w-full mx-auto top-0 left-0 flex items-center justify-between gap-2 px-2 py-2 text-1xl">
+                <p className="text-xl">Notice {notice?.id}</p>
+                <span className="grow flex flex-col gap-2 mx-1 my-auto" ><hr /><hr /><hr /></span>
+                <Link to="/" className="white-box-border rounded hover:bg-blue"><FontAwesomeIcon className="text-end px-1.5" icon={faXmark} /></Link>
             </div>
             <h2 className="text-3xl break-words mt-12 mx-6 text-center">{notice?.title}</h2>
           <div className="w-full p-4 bg-darkblue text-white rounded-xl mx-auto white-box-border">
