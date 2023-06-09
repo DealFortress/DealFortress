8c688db445ac1bb9f9359ff32c8f13c62e2424e4
Atte <pitkasenatte@gmail.com>
2023-06-09 13:44:47 +0200

    fixes too wide section in noticepage

--- frontend/src/pages/NoticePage.tsx
+++ frontend/src/pages/NoticePage.tsx
@@ -1,6 +1,9 @@
 import { useParams } from "react-router-dom"
 import { Notice } from "../types"
 import { NoticeProductCard } from "../component/Notice/NoticeProductCard"
+import { Main } from "../component/Main"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faCashRegister, faCity, faMoneyBill, faTractor, faTruckRampBox } from "@fortawesome/free-solid-svg-icons"
 
 type Props = {
   notices: Notice[]
@@ -18,26 +21,30 @@ export const NoticePage = ( {notices} : Props) => {
 
   const ProductsJSX = notice?.products.map( product => <NoticeProductCard key={product.id} product={product} />)
   return (
-    <div className="grid grid-cols-[3fr_6fr] gap-5">
-      <aside className="bg-white p-4 flex flex-col justify-between">
-        <div>
-          <h2 className="text-3xl mb-5">{notice?.title}</h2>
-          <p>{notice?.description}</p>
-        </div>
-        <div className="">
-          <ul className="font-start">
-            <li>{notice?.city}</li>
-            <li>{notice?.deliveryMethod}</li>
-          </ul>
-          <ul className="text-end">
-            <li className="font-bold">{}SEK</li>
-            <li>{notice?.payment}</li>
-          </ul>
-        </div>
-      </aside>
-      <section className="bg-white p-4 flex flex-col rounded gap-4">
-        {ProductsJSX}
-      </section>
-    </div>
+    <Main>
+      <div className="flex flex-col gap-4">
+        <section className="bg-darkblue rounded-lg p-4 flex flex-col justify-between ">
+          <div>
+            <h2 className="text-3xl mb-5 truncate">{notice?.title}</h2>
+            <p>{notice?.description}</p>
+          </div>
+          <div className="">
+            <ul className="font-start">
+              
+              <li><FontAwesomeIcon icon={faCity} /> {notice?.city}</li>
+              <li><FontAwesomeIcon icon={faTruckRampBox}/> {notice?.deliveryMethod}</li>
+            </ul>
+            <ul className="text-end">
+              <li className="font-bold">{}SEK</li>
+              <li>{notice?.payment} <FontAwesomeIcon icon={faCashRegister}/></li>
+            </ul>
+          </div>
+        </section>
+        <section className="bg-darkblue p-4 flex flex-col rounded gap-4">
+          <h3>Items</h3>
+          {/* {ProductsJSX} */}
+        </section>
+      </div>
+    </Main>
   )
 }
