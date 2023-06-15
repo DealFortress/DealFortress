d6c7c79466aff0cfb8abcfac84691c981556a012
atte <pitkasenatte@gmail.com>
2023-06-15 11:57:48 +0200

    Cleans API program.cs and tests

--- server/DealFortress.Api/Program.cs
+++ server/DealFortress.Api/Program.cs
@@ -4,9 +4,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 var builder = WebApplication.CreateBuilder(args);
 
-// builder.Services.AddDbContext<DealFortressContext>(options =>
-//     options.UseSqlServer(builder.Configuration.GetConnectionString("DealFortressContext") ?? throw new InvalidOperationException("Connection string 'DealFortressContext' not found.")));
-
 builder.Services.AddDbContext<DealFortressContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
