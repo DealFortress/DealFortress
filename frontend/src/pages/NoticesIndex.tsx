e2c8f7ae8eb026e67a6c6dc4cdf17d3329e2f466
atte <pitkasenatte@gmail.com>
2023-06-20 16:53:52 +0200

    adds createNotice button

--- frontend/src/pages/NoticesIndex.tsx
+++ frontend/src/pages/NoticesIndex.tsx
@@ -3,6 +3,9 @@ import { NoticeCard } from "../component/Notice/NoticeCard"
 import { Notice } from "../types"
 import logo from '../assets/logo.png'
 import { convertDateToMinutes } from "../services/helperFunctions"
+import { Link } from "react-router-dom"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faCross, faPlus, faPlusCircle } from "@fortawesome/free-solid-svg-icons"
 
 type Props = {
     notices : Notice[]
@@ -18,7 +21,11 @@ export const NoticesIndex = ( {notices} : Props ) => {
         </div>
         <MainContainer>
         <header>
-            <h2 className="text-2xl medieval-font">Latest posts</h2>
+            <div className="flex justify-between items-center w-full">
+                <h2 className="text-2xl medieval-font">Latest posts</h2>
+                <Link to="/createnotice" className="text-3xl"><FontAwesomeIcon icon={faPlusCircle}/></Link>
+            </div>
+            <p className="text-end">Create Notice</p>
         </header>
         <section className="flex flex-col gap-3">
             {NoticesJSX}
