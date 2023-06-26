71b37040b6be35472a9de081d13b86f0f4960e31
Luc-Siat <lucsiat@gmail.com>
2023-06-26 09:30:54 +0200

    fix centering on page with image

--- frontend/src/pages/UnderConstruction.tsx
+++ frontend/src/pages/UnderConstruction.tsx
@@ -7,7 +7,7 @@ export const UnderConstruction = () => {
         <MainContainer>
         <StyledContainer barText="Not found" redirectLink="/">
             <>
-              <img src={underConstruction} alt="under construction image" className=" w-96 h-96 py-2.5 mt-5" />
+              <img src={underConstruction} alt="under construction image" className=" w-96 h-96 py-2.5 mt-5 mx-auto" />
               <p className="text-center w-full text-4xl mb-6">Page under construction please tread carefully!</p>
             </>
         </StyledContainer>
