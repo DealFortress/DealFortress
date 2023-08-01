e30b35ef34e76ef01b477661e89f000e6bd3cd9f
atte <pitkasenatte@gmail.com>
2023-08-01 15:28:09 +0200

    Implements GetDTOById_returns_response_when_repo_returns_a_category test

--- server/src/Modules/Categories/DealFortress.Modules.Categories.Tests.Unit/Services/ServicesTestsHappy.cs
+++ server/src/Modules/Categories/DealFortress.Modules.Categories.Tests.Unit/Services/ServicesTestsHappy.cs
@@ -5,6 +5,7 @@ using DealFortress.Modules.Categories.Api.Controllers;
 using Moq;
 using DealFortress.Modules.Categories.Core.Domain.Repositories;
 using FluentAssertions;
+using DealFortress.Modules.Categories.Core.Domain.Entities;
 
 namespace DealFortress.Modules.Categories.Tests.Unit;
 
@@ -14,6 +15,7 @@ public class ServicesTestsHappy
     private readonly Mock<ICategoriesRepository> _repo;
     private readonly CategoryRequest _request;
     private readonly CategoryResponse _response;
+    private readonly Category _category;
 
     public ServicesTestsHappy()
     {
@@ -24,16 +26,49 @@ public class ServicesTestsHappy
         _request = new CategoryRequest() { Name = "test" };
 
         _response = new CategoryResponse() { Name = "test" };
+
+        _category = new Category() { Id = 1, Name = "test" };
     }
+
     [Fact]
     public void PostDTO_should_complete_to_save_to_db()
     {
-        // Arrange
-        var completed = false;
-         _repo.Setup(repo => repo.Complete()).Raises(f => completed = true);
         // Act
+        _service.PostDTO(_request);
 
         // Assert 
-        completed.Should().Be(true);
+        _repo.Verify(repo => repo.Complete(), Times.AtLeastOnce());
+
+    }
+
+    [Fact]
+    public void GetDTOById_returns_response_when_repo_returns_a_category()
+    {
+        // arrange
+        _repo.Setup(repo => repo.GetById(1)).Returns(_category);
+
+        // act
+        var response = _service.GetDTOById(1);
+
+        // assert
+        response.Should().BeOfType(typeof(CategoryResponse));
+    }
+
+    [Fact]
+    public void dsa()
+    {
+
+    }
+
+    [Fact]
+    public void sda()
+    {
+
+    }
+
+    [Fact]
+    public void sas()
+    {
+
     }
 }
