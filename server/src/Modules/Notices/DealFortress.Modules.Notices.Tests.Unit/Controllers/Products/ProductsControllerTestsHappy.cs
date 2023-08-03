2bf2361f10f6bc366b81323931dedaa88928f8d4
atte <pitkasenatte@gmail.com>
2023-08-03 11:33:53 +0200

    Implements Happy tests for ProductsController in NoticesModule

--- server/src/Modules/Notices/DealFortress.Modules.Notices.Tests.Unit/Controllers/Products/ProductsControllerTestsHappy.cs
+++ server/src/Modules/Notices/DealFortress.Modules.Notices.Tests.Unit/Controllers/Products/ProductsControllerTestsHappy.cs
@@ -101,58 +101,38 @@ public class ProductControllersTestsHappy
         httpResponses.Result.Should().BeOfType<OkObjectResult>();
     }
 
-    // [Fact]
-    // public void GetProducts_return_list_of_response_when_server_returns_responses()
-    // {
-    //     // Arrange
-    //     var list = new List<ProductResponse> { _response };
-    //     _service.Setup(service => service.GetAllDTO()).Returns(list);
-    //     // Act
-    //     var httpResponses = _controller.GetProducts();
-    //     // Assert 
-    //     var content = httpResponses.Result.As<OkObjectResult>().Value;
-    //     content.Should().BeOfType<List<ProductResponse>>();
-    // }
-    // [Fact]
-    // public void GetProduct_return_ok_when_service_return_response()
-    // {
-    //     // Arrange
-    //     _service.Setup(service => service.GetDTOById(1)).Returns(_response);
-    //     // Act
-    //     var httpResponse = _controller.GetProduct(1);
-    //     // Assert 
-    //     httpResponse.Result.Should().BeOfType<OkObjectResult>();
-    // }
+    [Fact]
+    public void GetProducts_return_list_of_response_when_server_returns_responses()
+    {
+        // Arrange
+        var list = new List<ProductResponse> { _response };
+        _service.Setup(service => service.GetAllDTO()).Returns(list);
+        // Act
+        var httpResponses = _controller.GetProducts();
+        // Assert 
+        var content = httpResponses.Result.As<OkObjectResult>().Value;
+        content.Should().BeOfType<List<ProductResponse>>();
+    }
 
-    // [Fact]
-    // public void GetProduct_return_response_when_server_returns_response()
-    // {
-    //     // Arrange
-    //     _service.Setup(service => service.GetDTOById(1)).Returns(_response);
-    //     // Act
-    //     var httpResponse = _controller.GetProduct(1);
-    //     // Assert 
-    //     var content = httpResponse.Result.As<OkObjectResult>().Value;
-    //     content.Should().BeOfType<ProductResponse>();
-    // }
-    // [Fact]
-    // public void PutProduct_return_no_content_when_service_return_response()
-    // {
-    //     // Arrange
-    //     _service.Setup(service => service.PutDTOById(1, _request)).Returns(_response);
-    //     // Act
-    //     var httpResponse = _controller.PutProduct(1, _request);
-    //     // Assert 
-    //     httpResponse.Should().BeOfType<NoContentResult>();
-    // }
-    // [Fact]
-    // public void DeleteProduct_should_return_no_content()
-    // {
-    //     // Arrange
-    //     _service.Setup(service => service.DeleteById(1)).Returns(_Product);
-    //     // Act
-    //     var httpResponse = _controller.DeleteProduct(1);
-    //     // Assert 
-    //     httpResponse.Should().BeOfType<NoContentResult>();
-    // }
+    [Fact]
+    public void PutProduct_return_no_content_when_service_return_response()
+    {
+        // Arrange
+        _service.Setup(service => service.PutDTOById(1, _request)).Returns(_response);
+        // Act
+        var httpResponse = _controller.PutProduct(1, _request);
+        // Assert 
+        httpResponse.Should().BeOfType<NoContentResult>();
+    }
+
+    [Fact]
+    public void DeleteProduct_should_return_no_content()
+    {
+        // Arrange
+        _service.Setup(service => service.DeleteById(1)).Returns(_Product);
+        // Act
+        var httpResponse = _controller.DeleteProduct(1);
+        // Assert 
+        httpResponse.Should().BeOfType<NoContentResult>();
+    }
 }
