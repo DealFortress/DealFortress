06ef5a2d292dcdcfcb0ddc41d6b96571490b7b66
atte <pitkasenatte@gmail.com>
2023-08-08 12:07:41 +0200

    Adds new migration for NoticesModule to pluralize DeliveryAndPayments

---
+++ server/src/Modules/Notices/DealFortress.Modules.Notices.Core/DAL/Migrations/20230808100509_PluralizePaymentsAndDelivery.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DealFortress.Modules.Notices.Core.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class PluralizePaymentsAndDelivery : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Payment",
+                schema: "noticesContext",
+                table: "Notices",
+                newName: "Payments");
+
+            migrationBuilder.RenameColumn(
+                name: "DeliveryMethod",
+                schema: "noticesContext",
+                table: "Notices",
+                newName: "DeliveryMethods");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Payments",
+                schema: "noticesContext",
+                table: "Notices",
+                newName: "Payment");
+
+            migrationBuilder.RenameColumn(
+                name: "DeliveryMethods",
+                schema: "noticesContext",
+                table: "Notices",
+                newName: "DeliveryMethod");
+        }
+    }
+}
