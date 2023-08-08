05a58f98ca04add32de286a41b17ac4f083d33d7
atte <pitkasenatte@gmail.com>
2023-08-08 15:00:56 +0200

    Adds Auth0Provider in main of frontend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,17 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools} from '@tanstack/react-query-devtools'
-import { App } from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { App } from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-const queryClient= new QueryClient();
+const queryClient = new QueryClient();
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-      <QueryClientProvider client={queryClient} >
+    <Auth0Provider
+      domain="dev-3ajyy6n01s37rnev.eu.auth0.com"
+      clientId="rXQINGs97eUOheZ0dzEAlXdZIGiQbla8"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+        <QueryClientProvider client={queryClient}>
           <App />
-          <ReactQueryDevtools/>
-      </QueryClientProvider>
-  </React.StrictMode>,
-)
+          <ReactQueryDevtools />
+        </QueryClientProvider>
+    </Auth0Provider>
+  </React.StrictMode>
+);
