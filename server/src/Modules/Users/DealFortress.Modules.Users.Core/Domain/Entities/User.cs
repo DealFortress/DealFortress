67116a5ab20a33bf65e077a6d8755b75973c4bc4
Luc-Siat <lucsiat@gmail.com>
2023-09-05 15:25:20 +0200

    fix user namespace and change auth0id to email

--- server/src/Modules/Users/DealFortress.Modules.Users.Core/Domain/Entities/User.cs
+++ server/src/Modules/Users/DealFortress.Modules.Users.Core/Domain/Entities/User.cs
@@ -1,12 +1,12 @@
 using System.ComponentModel.DataAnnotations;
 
-namespace DealFortress.Modules.Users.DealFortress.Modules.Users.Core.Domain.Entities
+namespace DealFortress.Modules.Users.Core.Domain.Entities
 {
     public class User
     {
         [Key]
         public int Id { get; set; }
-        public required string Auth0Id { get; set; }
+        public required string Email { get; set; }
         public required string Username { get; set; }
         public required string Avatar { get; set; }
     }
