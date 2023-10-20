035aa9c04122efb10b77c948263e5cb3f5c03709
Luc-Siat <lucsiat@gmail.com>
2023-10-20 10:07:40 +0200

    implemenet GetUser

--- server/src/Modules/Users/DealFortress.Modules.Users.Api/Controllers/UsersController.cs
+++ server/src/Modules/Users/DealFortress.Modules.Users.Api/Controllers/UsersController.cs
@@ -1,11 +1,27 @@
-// using Microsoft.AspNetCore.Mvc;
+using DealFortress.Modules.Users.Core.DTO;
+using DealFortress.Modules.Users.Core.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 
-// namespace DealFortress.Modules.Users.Api.Controllers
-// {
-//     [ApiController]
-//     [Route("api/[controller]")]
-//     public class UsersController : ControllerBase
-//     {
-        
-//     }
-// }
+namespace DealFortress.Modules.Users.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController : ControllerBase
+{
+    public readonly UsersService _service;
+    public UsersController(UsersService usersService)
+    {
+        _service = usersService;
+    }
+
+    [HttpGet("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public ActionResult<UserResponse> GetUser(int id)
+    {
+    var response = _service.GetById(id);
+
+    return response is null ? NotFound() : Ok(response);
+    }
+}
