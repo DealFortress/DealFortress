0d47bc280f0af9b96b99bb34b1a6407025c6fd0a
Atte <pitkasenatte@gmail.com>
2023-10-20 11:25:36 +0200

    Adds AuthId to user model and DTOs and implements post endpoint in the usersController

--- server/src/Modules/Users/DealFortress.Modules.Users.Core/DTO/UserRequest.cs
+++ server/src/Modules/Users/DealFortress.Modules.Users.Core/DTO/UserRequest.cs
@@ -4,6 +4,7 @@ namespace DealFortress.Modules.Users.Core.DTO
 {
     public class UserRequest
     {
+        public required string AuthId { get; set; }
         public required string Email { get; set; }
         public required string Username { get; set; }
         public required string Avatar { get; set; }
--- server/src/Modules/Users/DealFortress.Modules.Users.Core/DTO/UserResponse.cs
+++ server/src/Modules/Users/DealFortress.Modules.Users.Core/DTO/UserResponse.cs
@@ -3,6 +3,7 @@ namespace DealFortress.Modules.Users.Core.DTO
 {
     public class UserResponse
     {
+        public required int Id { get; set; }
         public required string Email { get; set; }
         public required string Username { get; set; }
         public required string Avatar { get; set; }
