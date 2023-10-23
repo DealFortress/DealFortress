1ebd0dbf7f0ed71959223d5e8dd87ef3069010c2
Luc-Siat <lucsiat@gmail.com>
2023-10-23 11:54:31 +0200

    add migrations for adding userid in notice table

---
+++ server/src/Modules/Notices/DealFortress.Modules.Notices.Core/DAL/Migrations/20231023095322_ImplementUserIdInNoticeTable.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DealFortress.Modules.Notices.Core.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class ImplementUserIdInNoticeTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                schema: "noticesContext",
+                table: "Notices",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                schema: "noticesContext",
+                table: "Notices");
+        }
+    }
+}
