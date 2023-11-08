1eb3317712425a69450a817c1d318f52e6d533b5
Atte Pitkänen <118916517+attepitkaenen@users.noreply.github.com>
2023-11-08 14:18:04 +0100

    Fixes environment names to match github secrets

--- frontend/src/environments/environment.production.ts
+++ frontend/src/environments/environment.production.ts
@@ -1,8 +1,8 @@
 export const environment = {
     production: true,
-    apiServerUrl: process.env['apiServerUrl'],
-    auth0Domain: process.env['auth0Domain'],
-    auth0ClientId: process.env['auth0ClientId'],
-    redirectUri: process.env['redirectUri'],
-    auth0Audience: process.env['auth0Audience']
+    apiServerUrl: process.env['APISERVERURL'],
+    auth0Domain: process.env['AUTH0DOMAIN'],
+    auth0ClientId: process.env['AUTH0CLIENTID'],
+    redirectUri: process.env['REDIRECTURI'],
+    auth0Audience: process.env['AUTH0AUDIENCE']
 };
