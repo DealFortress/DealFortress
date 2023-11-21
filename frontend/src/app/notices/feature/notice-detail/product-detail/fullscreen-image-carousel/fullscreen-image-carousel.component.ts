947f07cb2bdfb0fbfa2ef186900b87ef4e82272c
Luc-Siat <lucsiat@gmail.com>
2023-11-21 10:57:44 +0100

    implement swipe and arrow navigation on carousel

--- frontend/src/app/notices/feature/notice-detail/product-detail/fullscreen-image-carousel/fullscreen-image-carousel.component.ts
+++ frontend/src/app/notices/feature/notice-detail/product-detail/fullscreen-image-carousel/fullscreen-image-carousel.component.ts
@@ -1,5 +1,10 @@
-import { Component, EventEmitter, Input, OnDestroy, OnInit, Output, Renderer2} from '@angular/core';
+import { Component, EventEmitter, HostListener, Input, OnDestroy, OnInit, Output, Renderer2} from '@angular/core';
 import { Image } from '@app/shared/models/image.model'
+
+export enum KEY_CODE {
+  RIGHT_ARROW = 39,
+  LEFT_ARROW = 37
+}
 
 @Component({
   selector: 'app-fullscreen-image-carousel',
@@ -8,6 +13,10 @@ import { Image } from '@app/shared/models/image.model'
 })
 
 export class FullscreenImageCarouselComponent implements OnInit, OnDestroy{
+
+  private swipeCoord?: [number, number];
+  private swipeTime?: number;
+
 
   constructor(private renderer: Renderer2) {}
 
@@ -44,4 +53,36 @@ export class FullscreenImageCarouselComponent implements OnInit, OnDestroy{
     this.renderer.removeClass(document.body, 'overflow-hidden');
   }
 
+  @HostListener('window:keyup', ['$event'])
+  keyEvent(event: KeyboardEvent) {
+    console.log(event);
+
+    if (event.keyCode === KEY_CODE.RIGHT_ARROW) {
+      this.goToPrevious();
+    }
+
+    if (event.keyCode === KEY_CODE.LEFT_ARROW) {
+      this.goToNext();
+    }
+  }
+
+swipe(e: TouchEvent, when: string): void {
+  const coord: [number, number] = [e.changedTouches[0].clientX, e.changedTouches[0].clientY];
+  const time = new Date().getTime();
+
+  if (when === 'start') {
+    this.swipeCoord = coord;
+    this.swipeTime = time;
+  } 
+  else if (when === 'end') {
+    const direction = [coord[0] - this.swipeCoord![0], coord[1] - this.swipeCoord![1]];
+    const duration = time - this.swipeTime!;
+
+    if (duration < 1000
+      && Math.abs(direction[0]) > 30
+      && Math.abs(direction[0]) > Math.abs(direction[1] * 3)) {
+        direction[0] < 0 ?  this.goToNext() : this.goToPrevious();
+    }
+  }
+}
 }
--- frontend/src/app/notices/feature/notice-detail/product-detail/image-carousel/image-carousel.component.ts
+++ frontend/src/app/notices/feature/notice-detail/product-detail/image-carousel/image-carousel.component.ts
@@ -1,5 +1,10 @@
-import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, HostListener, Input, OnDestroy, OnInit, Output } from '@angular/core';
 import { Image } from '@app/shared/models/image.model'
+
+export enum KEY_CODE {
+  RIGHT_ARROW = 39,
+  LEFT_ARROW = 37
+}
 
 @Component({
   selector: 'app-image-carousel',
@@ -11,6 +16,10 @@ export class ImageCarouselComponent {
   @Input({required: true}) images : Image[] = []
   @Output() toggleFullscreen = new EventEmitter<boolean>();
   currentIndex: number = 0;
+
+  private swipeCoord?: [number, number];
+  private swipeTime?: number;
+
 
   goToPrevious(): void {
     const isFirstSlide = this.currentIndex === 0;
@@ -29,6 +38,39 @@ export class ImageCarouselComponent {
 
   goToSlide(slideIndex: number): void {
     this.currentIndex = slideIndex;
+  }
+
+  @HostListener('window:keyup', ['$event'])
+  keyEvent(event: KeyboardEvent) {
+    console.log(event);
+
+    if (event.keyCode === KEY_CODE.RIGHT_ARROW) {
+      this.goToPrevious();
+    }
+
+    if (event.keyCode === KEY_CODE.LEFT_ARROW) {
+      this.goToNext();
+    }
+  }
+
+  swipe(e: TouchEvent, when: string): void {
+    const coord: [number, number] = [e.changedTouches[0].clientX, e.changedTouches[0].clientY];
+    const time = new Date().getTime();
+  
+    if (when === 'start') {
+      this.swipeCoord = coord;
+      this.swipeTime = time;
+    } 
+    else if (when === 'end') {
+      const direction = [coord[0] - this.swipeCoord![0], coord[1] - this.swipeCoord![1]];
+      const duration = time - this.swipeTime!;
+  
+      if (duration < 1000
+        && Math.abs(direction[0]) > 30
+        && Math.abs(direction[0]) > Math.abs(direction[1] * 3)) {
+          direction[0] < 0 ?  this.goToNext() : this.goToPrevious();
+      }
+    }
   }
 
 }
