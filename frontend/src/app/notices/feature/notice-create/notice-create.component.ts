fecb45bf137d08391d75cb5f1159f5d9610d41e2
Atte <pitkasenatte@gmail.com>
2023-11-23 16:21:37 +0100

    Fixes put for products inside the notice

--- frontend/src/app/notices/feature/notice-create/notice-create.component.ts
+++ frontend/src/app/notices/feature/notice-create/notice-create.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { postNoticeRequest } from '@app/notices/data-access/store/notices.actions';
 import { getUserLatestNoticeId } from '@app/notices/data-access/store/notices.selectors';
@@ -12,12 +13,38 @@ import { Store } from '@ngrx/store';
 })
 export class NoticeCreateComponent {
 
-  constructor(private store: Store, private router: Router) {}
+  constructor(private store: Store, private router: Router, private formBuilder: FormBuilder) {}
 
   submitRequest(postRequest: NoticeRequest) {
     this.store.dispatch(postNoticeRequest({request: postRequest}));
     this.navigateToNewNotice()
   }
+
+  noticeFormGroup = this.formBuilder.group({
+    userId: [],
+    title: ['', [Validators.required, Validators.minLength(10)]],
+    description: ['', [Validators.required, Validators.minLength(30)]],
+    city: ['', [Validators.required, Validators.minLength(1)]],
+    payments: [[''], [Validators.required, Validators.minLength(1)]],
+    deliveryMethods: [[''], [Validators.required, Validators.minLength(1)]],
+    productRequests: this.formBuilder.array([
+      this.formBuilder.group({
+        name: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(50)]],
+        price: [0, [Validators.required, Validators.min(0), Validators.max(100000)]],
+        isSold: [false],
+        isSoldSeparately: [false],
+        hasReceipt: [false],
+        warranty: [''],
+        categoryId: [0],
+        condition: [0],
+        imageRequests: this.formBuilder.array([
+          this.formBuilder.group({
+            url: ['', [Validators.required]]
+          })
+        ], [Validators.required, Validators.minLength(1)]),  
+      })
+    ], [Validators.required, Validators.minLength(1)])
+  });
 
   
   navigateToNewNotice() {
--- frontend/src/app/notices/feature/notice-edit/notice-edit.component.ts
+++ frontend/src/app/notices/feature/notice-edit/notice-edit.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-import { postNoticeRequest } from '@app/notices/data-access/store/notices.actions';
-import { getUserLatestNoticeId } from '@app/notices/data-access/store/notices.selectors';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { postNoticeRequest, putNoticeRequest } from '@app/notices/data-access/store/notices.actions';
+import { getNoticeById, getUserLatestNoticeId } from '@app/notices/data-access/store/notices.selectors';
 import { NoticeRequest } from '@app/shared/models/notice-request.model';
+import { Notice } from '@app/shared/models/notice.model';
 import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs-compat';
 
 @Component({
   selector: 'app-notice-edit',
@@ -11,13 +14,41 @@ import { Store } from '@ngrx/store';
   styleUrls: ['./notice-edit.component.css']
 })
 export class NoticeEditComponent {
+  id = this.route.snapshot.paramMap.get('id');
+  notice$ = this.store.select(getNoticeById(+this.id!));
 
-  constructor(private store: Store, private router: Router) {}
+  constructor(private store: Store, private router: Router, private formBuilder: FormBuilder, private route: ActivatedRoute) {}
 
-  submitRequest(postRequest: NoticeRequest) {
-    // this.store.dispatch(updateNoticeRequest(postRequest));
-    this.navigateToNewNotice()
+  submitRequest(putRequest: NoticeRequest) {
+    this.store.dispatch(putNoticeRequest({request: putRequest, noticeId: +this.id!}));
+    this.navigateToNewNotice();
   }
+
+  noticeFormGroup = this.formBuilder.group({
+    userId: [],
+    title: ['', [Validators.required, Validators.minLength(10)]],
+    description: ['', [Validators.required, Validators.minLength(30)]],
+    city: ['', [Validators.required, Validators.minLength(1)]],
+    payments: [[''], [Validators.required, Validators.minLength(1)]],
+    deliveryMethods: [[''], [Validators.required, Validators.minLength(1)]],
+    productRequests: this.formBuilder.array([
+      this.formBuilder.group({
+        name: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(50)]],
+        price: [0, [Validators.required, Validators.min(0), Validators.max(100000)]],
+        isSold: [false],
+        isSoldSeparately: [false],
+        hasReceipt: [false],
+        warranty: [''],
+        categoryId: [0],
+        condition: [0],
+        imageRequests: this.formBuilder.array([
+          this.formBuilder.group({
+            url: ['', [Validators.required]]
+          })
+        ], [Validators.required, Validators.minLength(1)]),  
+      })
+    ], [Validators.required, Validators.minLength(1)])
+  });
 
   
   navigateToNewNotice() {
