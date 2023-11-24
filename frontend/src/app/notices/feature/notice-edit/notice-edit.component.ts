8399c0ffb0b45d8a4262a0d52cc88245b2921b1c
Luc-Siat <lucsiat@gmail.com>
2023-11-24 12:10:51 +0100

    implement create product form prefilled form for editing notices

--- frontend/src/app/notices/feature/notice-edit/notice-edit.component.ts
+++ frontend/src/app/notices/feature/notice-edit/notice-edit.component.ts
@@ -5,7 +5,9 @@ import { putNoticeRequest } from '@app/notices/data-access/store/notices.actions
 import { getNoticeById, getUserLatestNoticeId } from '@app/notices/data-access/store/notices.selectors';
 import { NoticeRequest } from '@app/shared/models/notice-request.model';
 import { Product } from '@app/shared/models/product.model';
+import { Image } from '@app/shared/models/image.model';
 import { Store } from '@ngrx/store';
+import { Notice } from '@app/shared/models/notice.model';
 
 @Component({
   selector: 'app-notice-edit',
@@ -20,42 +22,49 @@ export class NoticeEditComponent {
   constructor(private store: Store, private router: Router, private formBuilder: FormBuilder, private route: ActivatedRoute) {
 
     this.notice$.subscribe(notice => {
-
-      this.noticeForm = this.formBuilder.group({
-        userId: [notice?.userId],
-        title: [notice?.title, [Validators.required, Validators.minLength(10)]],
-        description: [notice?.description, [Validators.required, Validators.minLength(30)]],
-        city: [notice?.city, [Validators.required, Validators.minLength(1)]],
-        payments: [notice?.payments, [Validators.required, Validators.minLength(1)]],
-        deliveryMethods: [notice?.deliveryMethods, [Validators.required, Validators.minLength(1)]],
-        productRequests: this.formBuilder.array([], [Validators.required, Validators.minLength(1)])
-      });
-
-      this.addProducts(this.noticeForm, notice!.products);
-
+      if (notice) {
+        this.noticeForm = this.createNoticeFormGroup(notice)
+      }
     })
-
   }
 
-  addProducts(form: FormGroup, products: Product[]) {
-    // return this.formBuilder.group({
-    //       name: [product.name],
-    //       price: [product.price],
-    //       isSold: [product.isSold],
-    //       isSoldSeparately: [product.isSoldSeparately],
-    //       hasReceipt: [product.hasReceipt],
-    //       warranty: [product.warranty],
-    //       categoryId: [product.categoryId],
-    //       condition: [product.condition],
-    //       imageRequests: this.formBuilder.array([
-    //         product.images.map(image => {
-    //           return this.formBuilder.group({
-    //             url: [image.url]
-    //           });
-    //         })
-    //       ]), 
-    //     });
-      }
+  createNoticeFormGroup(notice : Notice) {
+    return this.formBuilder.group({
+      userId: [notice?.userId],
+      title: [notice?.title, [Validators.required, Validators.minLength(10)]],
+      description: [notice?.description, [Validators.required, Validators.minLength(30)]],
+      city: [notice?.city, [Validators.required, Validators.minLength(1)]],
+      payments: [notice?.payments, [Validators.required, Validators.minLength(1)]],
+      deliveryMethods: [notice?.deliveryMethods, [Validators.required, Validators.minLength(1)]],
+      productRequests: this.formBuilder.array(this.createProductFormGroups(notice?.products), [Validators.required, Validators.minLength(1)])
+    });
+  }
+
+  createProductFormGroups(products: Product[]) {
+    return products.map(product => {
+       return this.formBuilder.group({
+            name: [product.name],
+            price: [product.price],
+            isSold: [product.isSold],
+            isSoldSeparately: [product.isSoldSeparately],
+            hasReceipt: [product.hasReceipt],
+            warranty: [product.warranty],
+            categoryId: [product.categoryId],
+            condition: [product.condition],
+            imageRequests: this.formBuilder.array(
+              this.createImagesFormGroups(product.images)
+            ), 
+        });
+    })
+  }
+
+  createImagesFormGroups(images : Image[]) {
+    return images.map(image => {
+      return this.formBuilder.group({
+        url: [image.url]
+      });
+    })
+  }
       
   submitRequest(putRequest: NoticeRequest) {
     this.store.dispatch(putNoticeRequest({request: putRequest, noticeId: +this.id!}));
