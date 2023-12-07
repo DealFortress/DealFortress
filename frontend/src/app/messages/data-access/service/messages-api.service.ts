8cda3f9406d1bdac98bdadd0024d8990d31129ab
Luc-Siat <lucsiat@gmail.com>
2023-12-07 15:37:59 +0100

    removes ngx-signalr-websocket- gp

--- frontend/src/app/messages/data-access/service/messages-api.service.ts
+++ frontend/src/app/messages/data-access/service/messages-api.service.ts
@@ -4,7 +4,6 @@ import { Message } from '@app/shared/models/message';
 import { getUser } from '@app/users/data-access/store/users.selectors';
 import { Store } from '@ngrx/store';
 import { environment } from 'environments/environment.production';
-import { SignalrClient, SignalrConnection } from 'ngx-signalr-websocket';
 import * as signalr from '@microsoft/signalr'
 import { Subject } from 'rxjs';
 import { AuthService } from '@auth0/auth0-angular';
@@ -14,24 +13,14 @@ import { AuthService } from '@auth0/auth0-angular';
 })
 export class MessagesApiService {
   private messageHubUrl = `${environment.hubServerUrl}/messages-hub`;
-  private connection! : SignalrConnection;
 
-  constructor(private httpClient: HttpClient, private store: Store, public authService: AuthService,) {
+  constructor( private store: Store, public authService: AuthService,) {
       if (authService.isAuthenticated$)
       {
-        // SignalrClient
-        // .create(this.httpClient)
-        // .connect(this.messageHubUrl)
-        // .subscribe(connection => {
-        //   this.connection = connection
-        // });
+
         this.startConnection();
       }
   }
-
-  // startConnection(){
-  //   return this.connection.on<[]>("sendjointext")
-  // }
 
   startConnection() {
     const connection = new signalr
