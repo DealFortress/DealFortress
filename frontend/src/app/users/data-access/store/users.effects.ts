017a27833e2ef969e24e5f7fb76483e2b69247c6
Luc-Siat <lucsiat@gmail.com>
2023-12-08 15:23:56 +0100

    update package

--- frontend/src/app/users/data-access/store/users.effects.ts
+++ frontend/src/app/users/data-access/store/users.effects.ts
@@ -1,7 +1,7 @@
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { catchError, delay, map, mergeMap, tap} from 'rxjs/operators';
+import { catchError, delay, exhaustMap, map, mergeMap, tap} from 'rxjs/operators';
 import { Injectable } from '@angular/core';
-import { of } from 'rxjs';
+import { EMPTY, of } from 'rxjs';
 import { EmptyAction, ShowAlert } from '@app/shared/store/app.actions';
 import { UsersApiService } from '../services/users-api.service';
 import { loadUserByAuthIdError, loadUserByAuthIdRequest, loadUserByAuthIdSuccess, loadUserByIdError, loadUserByIdRequest, loadUserByIdSuccess, postUserError, postUserRequest, postUserSuccess } from './users.actions';
@@ -18,72 +18,74 @@ export class UsersEffect {
         ) {}
 
 
-    loadUserByAuthId$ = createEffect(() => {
-        return this.actions$.pipe(
+    loadUserByAuthId$ = createEffect(() => 
+        this.actions$.pipe(
             ofType(loadUserByAuthIdRequest),
-            map((action) => {
+            exhaustMap(() => {
                 return this.usersApiService.getUserByAuthIdAPI(action.authId).pipe(
-                    mergeMap((user) => {
-                        ShowAlert({ message: `Welcome back squire ${user.username}!`, actionresult: 'pass' })
-                        return loadUserByAuthIdSuccess({user: user, statusCode: 200})
-                    }
-                    ),
-                    catchError((_error) => of(
-                            loadUserByAuthIdError({errorText: _error.message, statusCode: _error.status})
-                        )
+                    map((user) => (
+                        // ShowAlert({ message: `Welcome back squire ${user.username}!`, actionresult: 'pass' }),
+                        loadUserByAuthIdSuccess({user: user, statusCode: 200})
                     )
+
+                        
+                    
+                    )
+                    // catchError((_error) => EMPTY
+                    //         // loadUserByAuthIdError({errorText: _error.message, statusCode: _error.status}) 
+                    // )
                 );
             })
-        );
-    })
-
-    loadUserById$ = createEffect(() => {
-        return this.actions$.pipe(
-            ofType(loadUserByIdRequest),
-            mergeMap((action) => {
-                return this.usersApiService.getUserByIdAPI(action.id).pipe(
-                    map((user) => { 
-                        return loadUserByIdSuccess({user: user, statusCode: 200})
-                    }),
-                    catchError((_error) => of(
-                            loadUserByIdError({errorText: _error.message, statusCode: _error.status})
-                        )
-                    )
-                );
-            })
-        );
-    })
-
-
-
-    postUser$ = createEffect(() =>
-    this.actions$.pipe(
-        ofType(postUserRequest),
-        mergeMap(action =>
-            this.usersApiService.postUserAPI(action).pipe(
-                map(user => {
-                            ShowAlert({ message: 'Welcome Squire!', actionresult: 'pass' });
-                            return postUserSuccess({ user: user as User, statusCode: 201 });               
-                        }
-                    ),
-                    catchError((_error) => of(
-                            ShowAlert({ message: 'Failed to get user from server, please reconnect squire.', actionresult: 'fail' }),
-                            postUserError({ errorText: _error.message, statusCode: _error.status})
-                        )
-                    ),
-                )
-            )
         )
-    );
+    )
 
-    logoutUser = createEffect(() => 
-    this.actions$.pipe(
-        ofType(postUserError),
-        map(() => {
-            setTimeout(() => {
-                this.authService.logout({ logoutParams: { returnTo: document.location.origin } })
-            }, 5000);
-            return EmptyAction();
-        })
-    ))
+    // loadUserById$ = createEffect(() => {
+    //     return this.actions$.pipe(
+    //         ofType(loadUserByIdRequest),
+    //         tap((action) => {
+    //             return this.usersApiService.getUserByIdAPI(action.id).pipe(
+    //                 map((user) => { 
+    //                     return loadUserByIdSuccess({user: user, statusCode: 200})
+    //                 }),
+    //                 catchError((_error) => of(
+    //                         loadUserByIdError({errorText: _error.message, statusCode: _error.status})
+    //                     )
+    //                 )
+    //             );
+    //         })
+    //     );
+    // })
+
+
+
+    // postUser$ = createEffect(() =>
+    // this.actions$.pipe(
+    //     ofType(postUserRequest),
+    //     mergeMap(action =>
+    //         this.usersApiService.postUserAPI(action).pipe(
+    //             map(user => {
+    //                         ShowAlert({ message: 'Welcome Squire!', actionresult: 'pass' });
+    //                         return postUserSuccess({ user: user as User, statusCode: 201 });               
+    //                     }
+    //                 ),
+    //                 catchError((_error) => of(
+    //                         ShowAlert({ message: 'Failed to get user from server, please reconnect squire.', actionresult: 'fail' }),
+    //                         postUserError({ errorText: _error.message, statusCode: _error.status})
+    //                     )
+    //                 ),
+    //             )
+    //         )
+    //     )
+    // );
+
+    // logoutUser = createEffect(() => 
+    // this.actions$.pipe(
+    //     ofType(postUserError),
+    //     map(() => {
+    //         setTimeout(() => {
+    //             this.authService.logout({ logoutParams: { returnTo: document.location.origin } })
+    //         }, 5000);
+    //         return EmptyAction();
+    //     })
+    // ))
 }
