9a08206cfe4ef6c2db3bdc57879b2e8f6840b38b
Luc-Siat <lucsiat@gmail.com>
2023-12-08 15:26:30 +0100

    fix effect in user

--- frontend/src/app/users/data-access/store/users.effects.ts
+++ frontend/src/app/users/data-access/store/users.effects.ts
@@ -21,71 +21,69 @@ export class UsersEffect {
     loadUserByAuthId$ = createEffect(() => 
         this.actions$.pipe(
             ofType(loadUserByAuthIdRequest),
-            exhaustMap(() => {
+            mergeMap(action => {
                 return this.usersApiService.getUserByAuthIdAPI(action.authId).pipe(
                     map((user) => (
-                        // ShowAlert({ message: `Welcome back squire ${user.username}!`, actionresult: 'pass' }),
+                        ShowAlert({ message: `Welcome back squire ${user.username}!`, actionresult: 'pass' }),
                         loadUserByAuthIdSuccess({user: user, statusCode: 200})
+                        )
+                    ),
+                    catchError((_error) => of(
+                        loadUserByAuthIdError({errorText: _error.message, statusCode: _error.status}) 
                     )
-
-                        
-                    
                     )
-                    // catchError((_error) => EMPTY
-                    //         // loadUserByAuthIdError({errorText: _error.message, statusCode: _error.status}) 
-                    // )
                 );
             })
         )
     )
 
-    // loadUserById$ = createEffect(() => {
-    //     return this.actions$.pipe(
-    //         ofType(loadUserByIdRequest),
-    //         tap((action) => {
-    //             return this.usersApiService.getUserByIdAPI(action.id).pipe(
-    //                 map((user) => { 
-    //                     return loadUserByIdSuccess({user: user, statusCode: 200})
-    //                 }),
-    //                 catchError((_error) => of(
-    //                         loadUserByIdError({errorText: _error.message, statusCode: _error.status})
-    //                     )
-    //                 )
-    //             );
-    //         })
-    //     );
-    // })
+    loadUserById$ = createEffect(() => {
+        return this.actions$.pipe(
+            ofType(loadUserByIdRequest),
+            mergeMap((action) => {
+                return this.usersApiService.getUserByIdAPI(action.id).pipe(
+                    map((user) => { 
+                        return loadUserByIdSuccess({user: user, statusCode: 200})
+                    }),
+                    catchError((_error) => of(
+                            loadUserByIdError({errorText: _error.message, statusCode: _error.status})
+                        )
+                    )
+                );
+            })
+        );
+    })
 
 
 
-    // postUser$ = createEffect(() =>
-    // this.actions$.pipe(
-    //     ofType(postUserRequest),
-    //     mergeMap(action =>
-    //         this.usersApiService.postUserAPI(action).pipe(
-    //             map(user => {
-    //                         ShowAlert({ message: 'Welcome Squire!', actionresult: 'pass' });
-    //                         return postUserSuccess({ user: user as User, statusCode: 201 });               
-    //                     }
-    //                 ),
-    //                 catchError((_error) => of(
-    //                         ShowAlert({ message: 'Failed to get user from server, please reconnect squire.', actionresult: 'fail' }),
-    //                         postUserError({ errorText: _error.message, statusCode: _error.status})
-    //                     )
-    //                 ),
-    //             )
-    //         )
-    //     )
-    // );
+    postUser$ = createEffect(() =>
+    this.actions$.pipe(
+        ofType(postUserRequest),
+        mergeMap(action =>
+            this.usersApiService.postUserAPI(action).pipe(
+                map(user => {
+                            ShowAlert({ message: 'Welcome Squire!', actionresult: 'pass' });
+                            return postUserSuccess({ user: user as User, statusCode: 201 });               
+                        }
+                    ),
+                    catchError((_error) => of(
+                            ShowAlert({ message: 'Failed to get user from server, please reconnect squire.', actionresult: 'fail' }),
+                            postUserError({ errorText: _error.message, statusCode: _error.status})
+                        )
+                    ),
+                )
+            )
+        )
+    );
 
-    // logoutUser = createEffect(() => 
-    // this.actions$.pipe(
-    //     ofType(postUserError),
-    //     map(() => {
-    //         setTimeout(() => {
-    //             this.authService.logout({ logoutParams: { returnTo: document.location.origin } })
-    //         }, 5000);
-    //         return EmptyAction();
-    //     })
-    // ))
+    logoutUser = createEffect(() => 
+    this.actions$.pipe(
+        ofType(postUserError),
+        map(() => {
+            setTimeout(() => {
+                this.authService.logout({ logoutParams: { returnTo: document.location.origin } })
+            }, 5000);
+            return EmptyAction();
+        })
+    ))
 }
