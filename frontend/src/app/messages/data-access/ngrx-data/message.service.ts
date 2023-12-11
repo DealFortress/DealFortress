e3f6a72d8e49daefcaeec549ae5b1b0583fa8a7a
Atte <pitkasenatte@gmail.com>
2023-12-11 11:13:56 +0100

    Removes message-api.service

--- frontend/src/app/messages/data-access/ngrx-data/message.service.ts
+++ frontend/src/app/messages/data-access/ngrx-data/message.service.ts
@@ -10,23 +10,58 @@ import { Store } from '@ngrx/store';
 import { environment } from 'environments/environment.production';
 import { Observable, of } from 'rxjs';
 import * as signalr from '@microsoft/signalr';
-import { MessagesApiService } from '../service/messages-api.service';
 
  
 @Injectable({ providedIn: 'root' })
 export class MessageService extends EntityCollectionServiceBase<Message> {
+  private messageHubUrl = `${environment.hubServerUrl}/messages-hub`;
+  private options!: signalr.IHttpConnectionOptions;
+  private connection? : signalr.HubConnection;
 
-  constructor(serviceElementsFactory: EntityCollectionServiceElementsFactory,private messagesApiService : MessagesApiService) {
+  constructor(serviceElementsFactory: EntityCollectionServiceElementsFactory, public authService: AuthService) {
     super('Message', serviceElementsFactory);
+
+    authService.getAccessTokenSilently().subscribe(token => {
+      this.options = {
+        accessTokenFactory: () => {
+          return token;
+        }
+      };
+    })
+
+    authService.isAuthenticated$.subscribe(isAuthenticated => {
+      if (isAuthenticated)
+      {
+        this.startConnection();
+      }
+    })
   }
 
-  // override getAll(){
-  //   this.messagesApiService.getAll()
-  //   return this.entities$;
-  // }
+  override getAll(){
+    return this.entities$;
+  }
 
   addAll(messages : Message[]) {
-    this.entities$ = of(messages);
+    // this.connection?.on("sendmessages", (data : Message[]) => 
+    // this.addAll(data)
+    // );
+    // this.entities$ = of(messages);
+  }
+
+  
+  startConnection() {
+    const connection = new signalr
+      .HubConnectionBuilder()
+      .configureLogging(signalr.LogLevel.Information)
+      .withUrl(this.messageHubUrl, this.options)
+      .build();
+
+    connection.start()
+    // this.connection = connection;
+
+    connection.on("sendjointext", data => {
+      console.log(data);
+    });
   }
 
 }
