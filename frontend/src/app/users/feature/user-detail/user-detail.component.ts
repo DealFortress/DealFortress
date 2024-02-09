7f2f2afc7db1c9c2619dd81dbee0e743b79a0df6
Luc-Siat <lucsiat@gmail.com>
2024-02-09 16:08:11 +0100

    implement logic to get user in user detail

--- frontend/src/app/users/feature/user-detail/user-detail.component.ts
+++ frontend/src/app/users/feature/user-detail/user-detail.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { User } from '@app/shared/models/user/user.model';
 import { getUserById } from '@app/users/data-access/store/users.selectors';
+import { UsersService } from '@app/users/utils/services/users.service';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 
@@ -13,12 +14,15 @@ import { Observable } from 'rxjs';
 export class UserDetailComponent implements OnInit{
   user? : Observable<User | undefined>;
 
-  constructor(private store: Store, private route : ActivatedRoute) {}
+  constructor(private store: Store, private route : ActivatedRoute, private usersService : UsersService) {}
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(paramMap => {
+      console.log(paramMap)
       if (paramMap.get('id')) {
-        this.store.select(getUserById(+(paramMap.get('id'))!));
+        const id = +(paramMap.get('id')!);
+        this.usersService.loadUserById(id)
+        this.user = this.store.select(getUserById(id));
       }
     })
   }
