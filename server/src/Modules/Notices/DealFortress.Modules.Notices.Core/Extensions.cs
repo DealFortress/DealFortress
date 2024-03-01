eba952abcb64b75b33959f83415108853bb8132f
Luc-Siat <lucsiat@gmail.com>
2024-03-01 10:59:58 +0100

    add IElasticClient to Notice AddCore extension

--- server/src/Modules/Notices/DealFortress.Modules.Notices.Core/Extensions.cs
+++ server/src/Modules/Notices/DealFortress.Modules.Notices.Core/Extensions.cs
@@ -6,8 +6,11 @@ using DealFortress.Modules.Notices.Core.Domain.Data;
 using DealFortress.Modules.Notices.Core.Domain.Repositories;
 using DealFortress.Modules.Notices.Core.Domain.Services;
 using DealFortress.Modules.Notices.Core.Services;
+using Elasticsearch.Net;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Nest;
 
 [assembly: InternalsVisibleTo("DealFortress.Modules.Notices.Api")]
 
@@ -24,6 +27,17 @@ internal static class Extensions
             .AddScoped<INoticesService, NoticesService>()
             .AddScoped<IProductsService, ProductsService>()
             .AddScoped<IImagesService, ImagesService>()
-            .AddAutoMapper(typeof(AutoMappingNoticeProfiles).Assembly);
+            .AddAutoMapper(typeof(AutoMappingNoticeProfiles).Assembly)
+            .AddSingleton<IElasticClient>(sp => 
+            {
+                var config = sp.GetRequiredService<IConfiguration>();
+
+                var settings = new ConnectionSettings(
+                    config["elasticCloudId"], 
+                    new ApiKeyAuthenticationCredentials(config["elasticApiKey"])
+                );
+
+                return new ElasticClient(settings);
+            });
     }
 }
